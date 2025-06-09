c8ea98076ca8422a92d202633e968d59f6df269f
sergii <sergii.polishchuk@gmail.com>
2025-06-09 12:45:41 +0300

    cleanup

--- PayPlay.NetClient/Models/Requests/ExchangeRateRequests.cs
+++
@@ -1,8 +0,0 @@
-namespace PayPlay.NetClient.Models.Requests;
-
-public class GetExchangeRateRequest
-{
-    public string FromCurrency { get; set; } = string.Empty;
-    public string ToCurrency { get; set; } = string.Empty;
-    public decimal? Amount { get; set; }
-}
--- PayPlay.NetClient/Models/Requests/WalletRequests.cs
+++
@@ -1,8 +0,0 @@
-namespace PayPlay.NetClient.Models.Requests;
-
-public class ListWalletsRequest
-{
-    public int Page { get; set; } = 1;
-    public int PageSize { get; set; } = 20;
-    public string? Currency { get; set; }
-}
